4a8829201e3ba08a39e90d52949f2594d9cd1d28
Fujio Nakatsuka <com.nifty.cbm01799>
2020-03-12 09:31:03 +0900

    Initial Commit

---
+++ TestProject02/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestProject02
+//
+//  Created by 中塚富士雄 on 2020/03/12.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
